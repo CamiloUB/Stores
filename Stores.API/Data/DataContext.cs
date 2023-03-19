94c5b181d2f31fb52a68283b64f7178b6c24702d
CamiloUB <126737141+CamiloUB@users.noreply.github.com>
2023-03-19 12:09:40 -0500

    Commit Country-DataContext

---
+++ Stores.API/Data/DataContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Stores.Shared.Entities;
+
+namespace Stores.API.Data
+{
+       public class DataContext:DbContext
+    {
+
+        public DataContext(DbContextOptions<DataContext> options) :base(options)
+        {
+           
+        }
+
+        public DbSet<Country>Countries { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        
+            modelBuilder.Entity<Country>().HasIndex(x=> x.Name).IsUnique();
+        
+
+
+        }
+             
+
+    }
+}
