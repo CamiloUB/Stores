48c76de4c4c8b7c2320a81dc4cf9d471b453f9ee
CamiloUB <126737141+CamiloUB@users.noreply.github.com>
2023-03-20 16:45:23 -0500

    Commit Generic List

--- Stores.WEB/Program.cs
+++ Stores.WEB/Program.cs
@@ -1,11 +1,14 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Stores.WEB;
+using Stores.WEB.Repositories;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7278/") });
+
+builder.Services.AddScoped<IRepository, Repository>();
 
 await builder.Build().RunAsync();
